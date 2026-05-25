62204c6638ff3a41fb68384186972245be9bc0a5
FBS019 <fbsantos019@gmail.com>
2026-05-25 10:35:08 -0300

    Add routing for Login and Cadastro components

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom' 
 import './index.css'
 import App from './App.jsx'
+import Login from './Login.jsx'       
+import Cadastro from './Cadastro.jsx' 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
