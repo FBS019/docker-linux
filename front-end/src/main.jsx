8a24924c0adbe5ca9ef638bc053cf64bb4c57957
FBS019 <fbsantos019@gmail.com>
2026-06-03 10:27:11 -0300

    Rename import paths for Login and Cadastro components

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import Login from './Login.jsx'
-import Cadastro from './Cadastro.jsx'
+import Login from './login.jsx'
+import Cadastro from './cadastro.jsx'
 
 
 createRoot(document.getElementById('root')).render(
